348f1c9ff6522b55080b26b2e8374f785075eb50
Anna Svetlova <svetloovaanna@gmail.com>
2022-05-08 21:47:18 +0300

    addcard

--- react-project/src/components/StudyCard.jsx
+++ react-project/src/components/StudyCard.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
+import StudyCardStyles from "../assets/styles/StudyCard.module.css";
+import CardButton from "../components/CardButton"
+import Translate from "../components/Translate"
 
 function StudyCard () {
+    const [button, clickButton] = useState(<CardButton / >);
+
+    const handleClick = () => {
+        clickButton(<Translate />)
+    };
+
     return (
-        <div className="wrapper">
+        <div className={StudyCardStyles.wrapper}>
+            <div className={StudyCardStyles.word_info}>
+                <div className={StudyCardStyles.word}>carrot</div>
+                <div className={StudyCardStyles.transcription}>[ ˈkærət ]</div>
+            </div>
+            {/* <button className={StudyCardStyles.btn} onClick={handleClick}>Проверить</button> */}
+
+            <div onClick={handleClick}>{button}</div>
             
         </div>
     )
---
+++ react-project/src/components/Translate.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CardButtonStyles from '../assets/styles/CardButton.module.css';
+
+
+
+function Translate () {
+    return (
+        <div className={CardButtonStyles.translate}>Морковь</div>
+    )
+}
+
+export default Translate
