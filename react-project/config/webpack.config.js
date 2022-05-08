4a7a1bb3858f258eb81904d21f726b848d516428
Anna Svetlova <svetloovaanna@gmail.com>
2022-05-08 19:40:09 +0300

    add changes

--- react-project/config/webpack.config.js
+++ react-project/config/webpack.config.js
@@ -28,47 +28,47 @@ const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin'
 
 const createEnvironmentHash = require('./webpack/persistentCache/createEnvironmentHash');
 
-var path = require('path');
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var values = require('postcss-modules-values');
+// var path = require('path');
+// var webpack = require('webpack');
+// var ExtractTextPlugin = require('extract-text-webpack-plugin');
+// var values = require('postcss-modules-values');
 
-module.exports = {
-  module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [{
-        loader: "style-loader" // creates style nodes from JS strings
-      }, {
-        loader: "css-loader" // translates CSS into CommonJS
-      }, {
-        loader: "sass-loader" // compiles Sass to CSS
-      }]
-    }]
-  }
-}; 
+// module.exports = {
+//   module: {
+//     rules: [{
+//       test: /\.scss$/,
+//       use: [{
+//         loader: "style-loader" // creates style nodes from JS strings
+//       }, {
+//         loader: "css-loader" // translates CSS into CommonJS
+//       }, {
+//         loader: "sass-loader" // compiles Sass to CSS
+//       }]
+//     }]
+//   }
+// }; 
 
-module.exports = {
-  entry: ['./src/index'],
-  output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'public'),
-    publicPath: '/public/'
-  },
-  module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader') }
-    ]
-  },
-  postcss: [
-    values
-  ],
-  plugins: [
-    new ExtractTextPlugin('style.css', { allChunks: true })
-  ]
+// module.exports = {
+//   entry: ['./src/index'],
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.join(__dirname, 'public'),
+//     publicPath: '/public/'
+//   },
+//   module: {
+//     loaders: [
+//       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+//       { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader') }
+//     ]
+//   },
+//   postcss: [
+//     values
+//   ],
+//   plugins: [
+//     new ExtractTextPlugin('style.css', { allChunks: true })
+//   ]
 
-};
+// };
 
 // Source maps are resource heavy and can cause out of memory issue for large source files.
 const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false';
