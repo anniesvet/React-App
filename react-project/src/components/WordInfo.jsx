2a183c932bca3cf6c5b084418291cd703206d54e
Anna Svetlova <svetloovaanna@gmail.com>
2022-05-17 23:32:27 +0300

    addChanges

--- react-project/src/components/WordInfo.jsx
+++ react-project/src/components/WordInfo.jsx
@@ -1,36 +1,42 @@
-import React from "react";
-import EditReadList from "./EditReadList";
-import ReadList from "./ReadList";
-
-let words = [
-    {id:1, english:"carrot",transcription:"[ ˈkærət ]",russian:"морковь", isEdit:false},
-    {id:2, english:"buttefly",transcription:"[ ˈbʌtəflaɪ ]",russian:"бабочка", isEdit:false},
-    {id:3, english:"street", transcription:"[ stri:t ]", russian:"улица", isEdit:false},
-    {id:4,english:"car",transcription:"[ kɑ: ]",russian:"автомобиль", isEdit:false},
-    {id:5,english:"arm",transcription:"[ ɑ:m ]",russian:"рука", isEdit:false},
-    {id:6,english:"air",transcription:"[  eər ]",russian:"воздух", isEdit:false},
-    {id:7,english:"fox",transcription:"[ fɒks ]",russian:"лиса", isEdit:false},
-    {id:8,english:"rabbit",transcription:"[ ˈræbɪt ]",russian:"кролик", isEdit:false},
-    {id:9,english:"owl",transcription:"[ aʊl ]",russian:"сова", isEdit:false},
-    {id:10,english:"mouse",transcription:"[ maʊs]",russian:"мышь", isEdit:false},
-    {id:11,english:"tea",transcription:"[ tiː ]",russian:"чай", isEdit:false},
-    {id:12,english:"goose",transcription:"[ ɡuːs ]",russian:"гусь", isEdit:false},
-    {id:13,english:"flower",transcription:"[ ˈflaʊər ]",russian:"цветок", isEdit:false},
-    {id:14,english:"bird",transcription:"[ be:d ]",russian:"птица", isEdit:false},
-    {id:15,english:"cat",transcription:"[ kæt ]",russian:"кот", isEdit:false}
-    ];
+import {React, useState} from "react";
+import { FaPenAlt } from 'react-icons/fa';
+import { FaTrashAlt } from 'react-icons/fa';
+import ReadListStyles from '../assets/styles/ReadList.module.css';
+import SaveButton from '../components/SaveButton'
+import saveButtonStyles from '../assets/styles/SaveButton.module.css';
 
 
 
-function WordInfo () {
+function WordInfo (props) {
+
+    const [isEdit, setIsEdit] = useState(true);
+
+    const handleIsEdit = () => {
+        setIsEdit(!isEdit) 
+    }
 
     return (
-        <div>
+        isEdit ?
+        (<div className={ReadListStyles.wrapper}> 
+            <div className={ReadListStyles.number}>{props.id}</div>
+            <div className={ReadListStyles.word}>{props.english}</div>
+            <div className={ReadListStyles.word}>{props.transcription}</div>
+            <div className={ReadListStyles.word}>{props.russian}</div>
+            <div className={ReadListStyles.edit}>
+                <button className={ReadListStyles.btn_edit} onClick={handleIsEdit}><FaPenAlt /></button>
+                <button className={ReadListStyles.btn_delete}><FaTrashAlt /></button>
+            </div>
+        </div>) :
         
-        {
-        words.map((word) => (
-            <ReadList id={word.id} english={word.english} transcription={word.transcription} russian={word.russian} />
-        ))}
+        <div className={ReadListStyles.wrapper}>
+            <div className={ReadListStyles.number}>{props.id}</div>
+            <div className={ReadListStyles.word}><input type="text"></input></div>
+            <div className={ReadListStyles.word}><input type="text"></input></div>
+            <div className={ReadListStyles.word}><input type="text"></input></div>
+            <div className={ReadListStyles.edit}>
+                <button className={ReadListStyles.btn_edit__save} onClick={handleIsEdit}>Сохранить</button>
+                <button className={ReadListStyles.btn_delete}><FaTrashAlt /></button>
+            </div>
         </div>
     )
 }
