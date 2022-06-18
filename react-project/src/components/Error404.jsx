e50e8c90e41e1d016817cc0e03e5f9bad3c18d88
Anna Svetlova <svetloovaanna@gmail.com>
2022-06-18 20:20:10 +0300

    add link to main page

--- react-project/src/components/Error404.jsx
+++ react-project/src/components/Error404.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import StudyCardStyles from "../assets/styles/StudyCard.module.css";
 
 function Error () {
     return (
-        <div className={StudyCardStyles.wrapper}>Ошибка 404!</div>
+        <div className={StudyCardStyles.wrapper}>Ошибка 404! Такой страницы не существует. Советуем посетить <Link to='/'>главную страницу</Link></div>
     )
 }
 
