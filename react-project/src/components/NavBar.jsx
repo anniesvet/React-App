956bac40926ec2fb4acae1f39537594cfeb267d6
Anna Svetlova <svetloovaanna@gmail.com>
2022-06-19 21:16:29 +0300

    flaticon

--- react-project/src/components/NavBar.jsx
+++ react-project/src/components/NavBar.jsx
@@ -5,7 +5,7 @@ import HeaderStyle from "../assets/styles/Header.module.css";
 function NavBar() {
     return(
         <nav className={HeaderStyle.navbar}>
-            <Link className={HeaderStyle.img} to="/"><img src="https://cdn-icons.flaticon.com/png/512/5486/premium/5486259.png?token=exp=1655555875~hmac=3f5944746ba6e7d2caec9541d773b344" alt="logo"></img></Link>
+            <Link className={HeaderStyle.img} to="/"><img src="https://cdn-icons.flaticon.com/png/512/5486/premium/5486259.png?token=exp=1655663452~hmac=4be79b0891a0c507c684b86dcf1dfa0e" alt="logo"></img></Link>
             <Link className={HeaderStyle.link} to="/">Главная</Link>
             <Link className={HeaderStyle.link} to="/game">Запоминать слова</Link>
         </nav>
