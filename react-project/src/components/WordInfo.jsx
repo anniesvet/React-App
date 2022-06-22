d77091319eaa7c6e4f4e33ad14dbec7e0650c28e
Anna Svetlova <svetloovaanna@gmail.com>
2022-06-22 19:14:06 +0300

    add style

--- react-project/src/components/WordInfo.jsx
+++ react-project/src/components/WordInfo.jsx
@@ -15,9 +15,23 @@ function WordInfo (props) {
     return (
             <div className={ReadListStyles.wrapper}> 
                 <div className={ReadListStyles.number}>{props.id}</div>
-                <div className={ReadListStyles.word}> {isEdit ? props.english : <input type="text" value={props.english} onChange={()=> {console.log(props.english)}} />} </div>
-                <div className={ReadListStyles.word}>{isEdit ? props.transcription : <input type="text" value={props.transcription} onChange={()=> {console.log(props.transcription)}}/>}</div>
-                <div className={ReadListStyles.word}>{isEdit ? props.russian : <input type="text" value={props.russian} onChange={()=> {console.log(props.russian)}} />}</div>
+                <div className={ReadListStyles.word}> {isEdit ? props.english : <input type="text"
+                value={props.english}
+                onChange={()=> {console.log(props.english)}} />}
+                </div>
+
+                <div className={ReadListStyles.word}>{isEdit ? props.transcription :
+                <input type="text"
+                value={props.transcription}
+                onChange={()=> {console.log(props.transcription)}}/>}
+                </div>
+
+                <div className={ReadListStyles.word}>{isEdit ? props.russian : <input type="text"
+                value={props.russian}
+                onChange={()=> {console.log(props.russian)}}
+                />}
+                </div>
+                
                 <div className={ReadListStyles.edit}>
                     <button className={isEdit ? ReadListStyles.btn_edit : ReadListStyles.btn_edit__save} onClick={handleIsEdit}>{isEdit && <FaPenAlt />}{!isEdit && "Сохранить"}</button>
                     {isEdit && <button className={ReadListStyles.btn_delete}><FaTrashAlt /></button>}
