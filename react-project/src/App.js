7915eb478aa6c66c9e0267fb0c96715a766cdddd
Anna Svetlova <svetloovaanna@gmail.com>
2022-07-20 01:55:11 +0300

    add fetch

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,4 +1,4 @@
-import {React, useState} from 'react';
+import {React, useState, useEffect} from 'react';
 import styles from './App.css';
 import Header from './components/Header';
 import Content from "./components/Content"
@@ -8,18 +8,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export default function App() {
 
-  let [words, setWords]  = useState([
-    {id:1, english:"carrot",transcription:"[ ˈkærət ]",russian:"морковь"},
-    {id:2, english:"buttefly",transcription:"[ ˈbʌtəflaɪ ]",russian:"бабочка"},
-    {id:3, english:"street", transcription:"[ stri:t ]", russian:"улица"},
-    {id:4,english:"car",transcription:"[ kɑ: ]",russian:"автомобиль"},
-    {id:5,english:"arm",transcription:"[ ɑ:m ]",russian:"рука"},
-    {id:6,english:"air",transcription:"[  eər ]",russian:"воздух"},
-    {id:7,english:"fox",transcription:"[ fɒks ]",russian:"лиса"},
-    {id:8,english:"rabbit",transcription:"[ ˈræbɪt ]",russian:"кролик"},
-    {id:9,english:"owl",transcription:"[ aʊl ]",russian:"сова"},
-    {id:10,english:"mouse",transcription:"[ maʊs]",russian:"мышь"}
-    ])
+  let [words, setWords]  = useState([])
+
+  useEffect(() => {
+    fetch('/api/words')
+    .then(response => response.json())
+    .then(response => setWords(response))
+  },[])
 
 
     let updateWordItem = (values) => {
